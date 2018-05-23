f64581aa9a9816cc7730eb1c3d1cd0c92178d151
Nupur Gunwant <nupurgunwant@Nupurs-MacBook-Pro.local>
2018-05-23 22:41:23 +0100

    made formatting changes according to PEP8

--- intermine/registry.py
+++ intermine/registry.py
@@ -2,12 +2,13 @@ import requests
 import yaml
 from intermine.webservice import Service
 
-def getInfo(mine):
-    link="http://registry.intermine.org/service/instances/" + mine
-    try:
-        r=requests.get(link)
 
-        dict=yaml.load(r.text)
+def getInfo(mine):
+    link = "http://registry.intermine.org/service/instances/" + mine
+    try:
+        r = requests.get(link)
+
+        dict = yaml.load(r.text)
         print("Description: " + dict["instance"]["description"])
         print("URL: " + dict["instance"]["url"])
         print("API Version: " + dict["instance"]["api_version"])
@@ -22,23 +23,24 @@ def getInfo(mine):
     except:
         print("No such mine available")
 
+
 def getData(mine):
-    x="http://registry.intermine.org/service/instances/" + mine
+    x = "http://registry.intermine.org/service/instances/" + mine
     try:
-        r=requests.get(x)
-        dict=yaml.load(r.text)
+        r = requests.get(x)
+        dict = yaml.load(r.text)
         link = dict["instance"]["url"]
-        service=Service(link)
+        service = Service(link)
         query = service.new_query("DataSet")
         query.add_view("name", "url")
-        list=[]
+        list = []
 
         for row in query.rows():
             try:
                 list.append(row["name"])
 
             except:
-                print ("No info available")
+                print("No info available")
         list.sort()
         for i in range(len(list)):
             print("Name: " + list[i])
@@ -47,18 +49,18 @@ def getData(mine):
 
 
 def getMines(organism):
-    link="http://registry.intermine.org/service/instances"
+    link = "http://registry.intermine.org/service/instances"
 
-    r=requests.get(link)
-    count=0
-    dict=yaml.load(r.text)
+    r = requests.get(link)
+    count = 0
+    dict = yaml.load(r.text)
     for i in range(len(dict["instances"])):
         for j in range(len(dict["instances"][i]["organisms"])):
-            if (dict["instances"][i]["organisms"][j]==organism):
+            if (dict["instances"][i]["organisms"][j] == organism):
                 print(dict["instances"][i]["name"])
-                count=count+1
-            elif (dict["instances"][i]["organisms"][j]== " " + organism):
+                count = count+1
+            elif (dict["instances"][i]["organisms"][j] == " " + organism):
                 print(dict["instances"][i]["name"])
-                count=count+1
-    if(count==0):
+                count = count+1
+    if(count == 0):
         print("No such mine available")
