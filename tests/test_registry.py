d5b408438e2a3c902f378b29ac7414540894e1f5
nupurgunwant <nupurgunwant2911@gmail.com>
2018-06-01 10:36:05 +0100

    changed according to pep8

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -4,30 +4,27 @@ import unittest
 
 class RegistryTest(unittest.TestCase):
 
-
     def test_getInfo(self):
-        #function returns none is everything runs fine
+        # function returns none is everything runs fine
         self.assertEqual(registry.getInfo('flymine'), None)
-        #function returns a message if anything goes wrong,
-        #example: mine is not correct
+        # function returns a message if anything goes wrong,
+        # example: mine is not correct
         self.assertEqual(registry.getData('flymin'), "No such mine available")
-
 
     def test_getData(self):
-        #function returns none is everything runs fine
+        # function returns none is everything runs fine
         self.assertEqual(registry.getData('flymine'), None)
-        #function returns a message if anything goes wrong,
-        #example: mine is not correct
+        # function returns a message if anything goes wrong,
+        # example: mine is not correct
         self.assertEqual(registry.getData('flymin'), "No such mine available")
 
-
     def test_getMines(self):
-        #function returns none is everything runs fine
+        # function returns none is everything runs fine
         self.assertEqual(registry.getMines('D. melanogaster'), None)
-        #function returns a message if anything goes wrong,
-        #example: organism name is not correct
-        self.assertEqual(registry.getMines('D. melanogaste'),\
-        "No such mine available")
+        # function returns a message if anything goes wrong,
+        # example: organism name is not correct
+        self.assertEqual\
+            (registry.getMines('D. melanogaste'), "No such mine available")
 
 
 if __name__ == '__main__':
