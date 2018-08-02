e528f2c9dbf3e7893dc17bec79732346a039340b
Nupur Gunwant <nupurgunwant@Nupurs-MacBook-Pro.local>
2018-08-02 18:47:18 +0530

    formatting changes

--- intermine/bar_chart.py
+++ intermine/bar_chart.py
@@ -73,10 +73,10 @@ def plot_go_vs_p(list_name):
 
     gene_count = []
     identifier = []
-    p_value=[]
+    p_value = []
     object_count = 0
     for i in r:
-        if object_count<5:
+        if object_count < 5:
             gene_count.append(i.matches)
             identifier.append(i.identifier)
             p_value.append(i.p_value)
@@ -92,27 +92,30 @@ def plot_go_vs_p(list_name):
     ax.set_title('GO Term vs p-value (Label: Gene count)')
     ax.set_xlabel('GO Term')
     ax.set_ylabel('p_value')
-    ax.set_xticklabels(x,rotation='horizontal')
+    ax.set_xticklabels(x, rotation='horizontal')
 
     rects = ax.patches
+
     def autolabel(rects, ax):
-        i=0
+        i = 0
         for rect in rects:
             x = rect.get_x() + rect.get_width()/2.
             y = rect.get_height()
-            ax.annotate(gene_count[i], (x,y), xytext=(0,5), textcoords="offset points",
-                        ha='center', va='bottom')
+            ax.annotate(gene_count[i], (x,y), xytext = (0,5),
+                        textcoords = "offset points",
+                        ha = 'center', va = 'bottom')
             i = i+1
 
-    autolabel(ax.patches,ax)
+    autolabel(ax.patches, ax)
 
-    ax.margins(y = 0.1)
+    ax.margins(y=0.1 )
     plt.show()
 
 
 def plot_go_vs_count(list_name):
     """
-    A function to plot GO Term vs gene count with label of annotation on each bar
+    A function to plot GO Term vs gene count with label of annotation
+    on each bar
     ================================================
     example:
 
@@ -128,8 +131,8 @@ def plot_go_vs_count(list_name):
     service = Service(url)
 
     lm = service.list_manager()
-    l = lm.get_list(name = list_name)
-    r = l.calculate_enrichment(widget = "go_enrichment_for_gene")
+    l = lm.get_list(name=list_name)
+    r = l.calculate_enrichment(widget="go_enrichment_for_gene")
 
     gene_count = []
     identifier = []
@@ -144,7 +147,7 @@ def plot_go_vs_count(list_name):
             annotation_count.append(i.populationAnnotationCount)
             object_count = object_count + 1
         else:
-            if object_count>=5:
+            if object_count >= 5:
                 break
 
     y = pd.Series(gene_count)
@@ -152,20 +155,22 @@ def plot_go_vs_count(list_name):
 
     # Plot the figure.
 
-    ax = y.plot(kind='bar')
+    ax = y.plot(kind = 'bar')
     ax.set_title('GO Term vs Count (Label: Annotation)')
     ax.set_xlabel('GO Term')
     ax.set_ylabel('Number of Genes')
-    ax.set_xticklabels(x,rotation='horizontal')
+    ax.set_xticklabels(x,rotation = 'horizontal')
 
     rects = ax.patches
+
     def autolabel(rects, ax):
         i=0
         for rect in rects:
             x = rect.get_x() + rect.get_width()/2.
             y = rect.get_height()
-            ax.annotate(annotation_count[i], (x,y), xytext=(0,5), textcoords="offset points",
-                        ha='center', va='bottom')
+            ax.annotate(annotation_count[i], (x,y), xytext = (0,5),
+                        textcoords = "offset points",
+                        ha = 'center', va = 'bottom')
             i=i+1
 
     autolabel(ax.patches,ax)
@@ -182,15 +187,16 @@ def get_query(xml):
     link = "http://registry.intermine.org/service/instances/" + mine
     r = requests.get(link)
     dict = json.loads(r.text)
-    link = dict["instance"]["url"] + "/service/query/results?query="+ req.pathname2url(xml)
+    link = dict["instance"]["url"] + "/service/query/results?query="+ \
+            req.pathname2url(xml)
     r=requests.get(link)
     list=(r.text).split('\n')
-    for i in range(0,len(list)-1):
+    for i in range(0, len(list)-1):
         list[i]=list[i].split('\t')
     return (list)
 
 
-def query_to_barchart_log(xml,resp):
+def query_to_barchart_log(xml, resp):
     """
     A function to plot a query from its xml
     NOTE: first argument:
@@ -204,7 +210,7 @@ def query_to_barchart_log(xml,resp):
     example:
 
         >>>from intermine import query_manager as qm
-        >>>b.query_to_barchart_log(<xml>,'true')
+        >>>b.query_to_barchart_log(<xml>, 'true')
         <plots the second column vs log(third column)>
 
     """
@@ -214,13 +220,13 @@ def query_to_barchart_log(xml,resp):
     l = l.split(' ')
     x_val = []
     y_val = []
-    for i in range(0,len(list)-1):
+    for i in range(0, len(list)-1):
         x_val.append(list[i][1])
         y_val.append(float(list[i][2]))
 
     if resp == 'true':
         y_val = np.log(y_val)
-        y_val = np.round_(y_val,2)
+        y_val = np.round_(y_val, 2)
 
     y = pd.Series(y_val)
     x = pd.Series(x_val)
@@ -232,19 +238,21 @@ def query_to_barchart_log(xml,resp):
         ax.set_ylabel('log('+ l[2] + ')')
     else:
         ax.set_ylabel(l[2])
-    ax.set_xticklabels(x,rotation='vertical')
+    ax.set_xticklabels(x, rotation='vertical')
 
     rects = ax.patches
+
     def autolabel(rects, ax):
         i=0
         for rect in rects:
             x = rect.get_x() + rect.get_width()/2.
             y = rect.get_height()
-            ax.annotate(y_val[i], (x,y), xytext=(0,5), textcoords="offset points",
-                        ha='center', va='bottom')
+            ax.annotate(y_val[i], (x,y), xytext = (0,5),
+                        textcoords = "offset points",
+                        ha = 'center', va = 'bottom')
             i = i+1
 
-    autolabel(ax.patches,ax)
+    autolabel(ax.patches, ax)
 
-    ax.margins(y = 0.1)
+    ax.margins(y=0.1)
     plt.show()
