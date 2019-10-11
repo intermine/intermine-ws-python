361fccab7c0b21ed8beb75003b7d7b4ab535dd00
TheAntiSnipe <mihir.prashant.kulkarni@gmail.com>
2019-10-11 22:04:02 +0530

    Update registry.py

--- intermine/registry.py
+++ intermine/registry.py
@@ -10,16 +10,16 @@ Functions for making use of registry data
 
 def getVersion(mine):
     """
-    A function to return the API version, release version and 
+    A function to return the API version, release version and
     InterMine version numbers
     ================================================
     example:
 
         >>> from intermine import registry
         >>> registry.getVersion('flymine')
-        >>> {'API Version:': '30', 'Release Version:': '48 2019 October', 
+        >>> {'API Version:': '30', 'Release Version:': '48 2019 October',
         'InterMine Version:': '4.1.0'}
-    
+        
     """
     link = "http://registry.intermine.org/service/instances/" + mine
     try:
