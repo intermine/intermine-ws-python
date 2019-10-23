ef92d1f405e8859cc6c1368c457073b74378b8ce
altffour <ayhamaboualfadl@gmail.com>
2019-10-23 20:35:29 +0300

    Apply PEP8 on sample

--- samples/alleles.py
+++ samples/alleles.py
@@ -1,25 +1,31 @@
 from intermine.webservice import Service
 import itertools
 
+
 def lines_of(x):
     return chunker(0, x)
 
+
 def chunker(i, x):
     d = {"accum": i}
+
     def func(y):
         i = d["accum"]
         grp = i / x
         i += 1
         d["accum"] = i
         return grp
+
     return func
+
 
 col_width = 15
 cols = 8
 sep = '| '
 ellipsis = '...'
 line_width = col_width * cols + (cols - 1) * len(sep)
-fit_to_cell = lambda a: a.ljust(col_width) if len(a) <= col_width else a[:col_width - len(ellipsis)] + ellipsis
+fit_to_cell = lambda a: a.ljust(col_width) if len(
+    a) <= col_width else a[:col_width - len(ellipsis)] + ellipsis
 hrule = "-" * line_width
 summary = "\n%s: %d Alleles"
 
@@ -35,20 +41,24 @@ q = s.query(Gene).\
       order_by("symbol")
 
 for row in q.rows():
-   print row
+    print row
 
-for gene in s.query(s.model.Gene).filter(s.model.Gene.symbol == ["zen", "eve", "bib", "h"]).add_columns(s.model.Gene.alleles):
+for gene in s.query(s.model.Gene).filter(
+        s.model.Gene.symbol == ["zen", "eve", "bib", "h"]).add_columns(
+            s.model.Gene.alleles):
 
     print summary % (gene.symbol, len(gene.alleles))
     print hrule
 
-    for k, line_of_alleles in itertools.groupby(sorted(map(lambda a: a.symbol, gene.alleles)), lines_of(cols)):
+    for k, line_of_alleles in itertools.groupby(
+            sorted(map(lambda a: a.symbol, gene.alleles)), lines_of(cols)):
         print sep.join(map(fit_to_cell, line_of_alleles))
-    
+
     print "\nAllele Classes:"
-    allele_classes = [(key, len(list(group))) for key, group in itertools.groupby(sorted(map(lambda x: x.alleleClass, gene.alleles)))]
+    allele_classes = [(key, len(list(group)))
+                      for key, group in itertools.groupby(
+                          sorted(map(lambda x: x.alleleClass, gene.alleles)))]
     for pair in reversed(sorted(allele_classes, key=lambda g: g[1])):
         print "%s (%d)" % pair
 
     print hrule
-
