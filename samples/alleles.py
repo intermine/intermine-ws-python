fa381bb593811c57533084e0f990157319d7ae59
Basil Asad <55938020+mbasil09@users.noreply.github.com>
2019-12-17 02:44:43 +0530

    pep8

--- samples/alleles.py
+++ samples/alleles.py
@@ -18,26 +18,6 @@ def chunker(i, x):
     return func
 
 
-col_width = 15from intermine.webservice import Service
-import itertools
-
-
-def lines_of(x):
-    return chunker(0, x)
-
-
-def chunker(i, x):
-    d = {"accum": i}
-
-    def func(y):
-        i = d["accum"]
-        grp = i / x
-        i += 1
-        d["accum"] = i
-        return grp
-    return func
-
-
 col_width = 15
 cols = 8
 sep = '| '
@@ -75,53 +55,6 @@ for gene in s.query(s.model.Gene).filter(filter_genes):
     iterhelper = itertools.groupby(sorted(map(lambda a: a.symbol,
                                               gene.alleles)), lines_of(cols))
     for k, line_of_alleles in (iterhelper):
-        print sep.join(map(fit_to_cell, line_of_alleles))
-
-    print "\nAllele Classes:"
-    allele_classes = [(key, len(list(group))) for
-                      key, group in itertools.groupby(
-        sorted(map(lambda x: x.alleleClass, gene.alleles)))]
-    for pair in reversed(sorted(allele_classes, key=lambda g: g[1])):
-        print "%s (%d)" % pair
-
-    print hrule
-cols = 8
-sep = '| '
-ellipsis = '...'
-line_width = col_width * cols + (cols - 1) * len(sep)
-
-
-def fit_to_cell(a): return a.ljust(col_width) if len(
-    a) <= col_width else a[:col_width - len(ellipsis)] + ellipsis
-
-
-hrule = "-" * line_width
-summary = "\n%s: %d Alleles"
-
-s = Service("www.flymine.org/query")
-
-Gene = s.model.Gene
-
-q = s.query(Gene).\
-      add_columns("name", "symbol", "alleles.*").\
-      filter(Gene.symbol == ["zen", "eve", "bib", "h"]).\
-      filter(Gene.alleles.symbol == "*hs*").\
-      outerjoin(Gene.alleles).\
-      order_by("symbol")
-
-for row in q.rows():
-    print row
-gene_symbols = ["zen", "eve", "bib", "h"]
-filter_genes = (s.model.Gene.symbol ==
-                (gene_symbols).add_columns(s.model.Gene.alleles))
-for gene in s.query(s.model.Gene).filter(filter_genes):
-
-    print summary % (gene.symbol, len(gene.alleles))
-    print hrule
-
-    for k, line_of_alleles in
-            itertools.groupby(sorted(map(lambda a: a.symbol,
-            gene.alleles)), lines_of(cols)):
         print sep.join(map(fit_to_cell, line_of_alleles))
 
     print "\nAllele Classes:"
