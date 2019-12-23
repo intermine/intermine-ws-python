3c5615f87ba6e22188bfe3f16179ffcb02268ec9
Basil Asad <55938020+mbasil09@users.noreply.github.com>
2019-12-24 00:14:51 +0530

    update to py-3

--- tests/live_test.py
+++ tests/live_test.py
@@ -7,9 +7,9 @@ from interminebio import RegionQuery, SequenceQuery
 s = Service("squirrel.flymine.org/flymine", token="C1o3t1e0d4V06ep8xb47DdlFVMr")
 q = RegionQuery(s, "D. melanogaster", ["Exon", "Intron"], ["2L:14614843..14619614", "Foo"])
 
-print q.bed()
-print q.fasta()
-print q.gff3()
+print (q.bed())
+print (q.fasta())
+print (q.gff3())
 
 l = s.create_list(q)
 
